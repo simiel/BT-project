e239e9d92eafedcef191d100780f8aae27b92653
smensah <simieldev@gmail.com>
2024-12-14 08:06:02 +0000

    chore: add config keys

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+    },
+  },
 };
