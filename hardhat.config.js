d11d61bc62e3afc7a43e6b7c70613d0d40f64b3d
smensah <simieldev@gmail.com>
2024-12-14 08:47:29 +0000

    chore: deployed to sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -7,6 +8,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
     },
   },
 };
